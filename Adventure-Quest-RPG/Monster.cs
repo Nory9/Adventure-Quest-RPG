2e4ed951135588081c47dcf28836f42275b45c2e
Nory9 <noormisk94@gmail.com>
2024-06-30 22:21:14 +0300

    character classes done

---
+++ Adventure-Quest-RPG/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure_Quest_RPG
+{
+    public class Monster : Character
+
+    {
+        public Monster(string name, int attackPower, int health, int defence) : base(name, health, attackPower, defence)
+        {
+        }
+        public Monster() : base()
+        {
+        }
+
+    }
+}
